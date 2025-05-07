aaf236f19e7e1cf4f36ccef55c49cec25418f8ad
Venki1402 <saivenkatesh.alampally@gmail.com>
2025-05-07 11:02:50 +0530

    init
    Signed-off-by: Venki1402 <saivenkatesh.alampally@gmail.com>

---
+++ src/main/java/com/example/cinewiki/CinewikiApplication.java
@@ -0,0 +1,12 @@
+package com.example.cinewiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinewikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinewikiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/cinewiki/CinewikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinewiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinewikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
